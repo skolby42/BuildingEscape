b3831aab9655fc2b1b298d1788b5c0419979adab
Scott Kolby <skolby42@gmail.com>
2025-07-15 14:30:39 -0700

    Fixes for 5.5.

--- Source/BuildingEscape.Target.cs
+++ Source/BuildingEscape.Target.cs
@@ -8,7 +8,7 @@ public class BuildingEscapeTarget : TargetRules
 	public BuildingEscapeTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "BuildingEscape" } );
 	}
 }
--- Source/BuildingEscapeEditor.Target.cs
+++ Source/BuildingEscapeEditor.Target.cs
@@ -8,7 +8,7 @@ public class BuildingEscapeEditorTarget : TargetRules
 	public BuildingEscapeEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "BuildingEscape" } );
 	}
 }
